c34de6537f975c840494a156b1948476b93d90d1
lucasdsks <lucasdrifitim@hotmail.com>
2021-08-25 21:16:41 -0300

    Primeiro Commit

---
+++ src/faculdadeUniderp/Main.java
@@ -0,0 +1,21 @@
+package faculdadeUniderp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		Cliente cliente = new Cliente("Lucas" , "Rua x");
+		Cliente cliente2 = new Cliente();
+		
+		cliente.setNome("Lucas");
+		
+		System.out.println(cliente);
+		System.out.println(cliente2);
+		System.out.println(cliente.getNome());
+		
+
+	}
+
+}
